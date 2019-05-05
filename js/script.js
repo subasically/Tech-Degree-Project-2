cc75d7989db63894be556d9bbb1bbf0b30fd7dd2
Alen Subasic <alensubasic@gmail.com>
2019-05-05 18:33:22 -0500

    push from vsc

--- js/script.js
+++ js/script.js
@@ -7,33 +7,19 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   Creating and storing the length of the student list and
+   setting the default page length used to determine how
+   many students to show per page.
 ***/
-const pageLength = 10;
 const list = document.getElementsByClassName('student-item');
+const pageLength = 10;
 
 
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   Uses the 'page' param to determine the START and END index
+   of the student list and then loops through the 'list' param
+   to display only the students within that index range
 ***/
 function showPage(list, page) {
    const start = (page * pageLength) - pageLength;
@@ -46,15 +32,18 @@ function showPage(list, page) {
          list[i].style.display = 'none';
       }
    }
-
    appendPageLinks(list);
-}
-
-showPage(list, 1);
+};
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   Call showPage function and pass in the student list and desired
+   page number to show.
+***/
+showPage(list, 1);
+
+
+/***
+   Take the 'list' param and create the pagination links
 ***/
 function appendPageLinks(list) {
    const page = document.getElementsByClassName('page');
@@ -89,9 +78,4 @@ function appendPageLinks(list) {
       li.appendChild(a);
       ul.appendChild(li);
    }
-}
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+};
